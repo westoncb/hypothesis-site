146e1074a197b41cc890e482071e0a4d3942ee65
Weston Beecroft <westoncb@gmail.com>
2014-07-06 01:06:42 -0700

    cells collapsing/expanding

--- src/scripts/hypothesis-renderer.js
+++ src/scripts/hypothesis-renderer.js
@@ -7,8 +7,7 @@ $(document).ready(function(){
 		var endIndex = 1;
 		for (var i = 0; i < number.length; i++) {
 			var character = number[i];
-			if ((typeof(character) === 'number' && typeof(character) !== NaN) || character === '-') {
-				console.log("asdfasdf");
+			if ((typeof(character) === 'number' && typeof(character) !== NaN) || character === '-' || character === '+' || character === '.') {
 				endIndex++;
 			} else
 				break;
@@ -77,22 +76,67 @@ $(document).ready(function(){
 	var cellContainerString = '<div class="cellcontainer"></div>';
 	const subCellShiftWidth = 50;
 
-	function walkJSON(json, depth, root, parent, cells) {
+	function buildComponentTree(json, root, parent, cells) {
 		if (json instanceof Array) {
 			for (var element in json) {
-				walkJSON(json[element], depth, root, parent, cells);	
+				buildComponentTree(json[element], root, parent, cells);	
 			}
 		} else {
 			var thisCell = $(cellString);
 			var thisContainer = $(cellContainerString); 
 			var thisCellLeftMargin = $(cellLeftMarginString);
+			var atTheRoot = (root === parent);
 
 			var node = {
 				container: thisContainer,
 				leftMargin: thisCellLeftMargin,
 				cell: thisCell,
-				parent: parent
+				parent: (atTheRoot ? 0 : parent),
+				childSet: [],
+				isOpen: function() {
+					if (this.childSet.length == 0)
+						return true;
+
+					var isit = this.childSet[0].container.is(":visible");
+					console.log(isit);
+					return isit;
+				},
+
+				recursiveHideChildren: function(func) {
+					for (var i = 0; i < this.childSet.length; i++) {
+						var child = this.childSet[i];
+						child.container.slideUp({progress: func,
+												 duration: 100});
+						child.recursiveHideChildren();
+					};
+				},
+
+				recursiveShowChildren: function() {
+					for (var i = 0; i < this.childSet.length; i++) {
+						var child = this.childSet[i];
+						child.container.show();
+						child.recursiveShowChildren();
+					};
+				},
+
+				showChildren: function(func) {
+					for (var i = 0; i < this.childSet.length; i++) {
+						var child = this.childSet[i];
+						child.container.slideDown({progress: func,
+												 duration: 100});
+					};	
+				},
+
+				hideChildren: function() {
+					for (var i = 0; i < this.childSet.length; i++) {
+						var child = this.childSet[i];
+						child.container.hide();
+					};	
+				}
 			};
+
+			if (node.parent.childSet)
+				node.parent.childSet.push(node);
 
 			thisContainer.append(thisCellLeftMargin);
 			thisContainer.append(thisCell);
@@ -104,7 +148,7 @@ $(document).ready(function(){
 
 		    	var value = json[key]
 		    	if (typeof(value) == "object") {
-	    			var subCells = walkJSON(value, depth+1, root, node, []);
+	    			var subCells = buildComponentTree(value, root, node, []);
 	    			cells.push(subCells);
 		    	} else {
     				thisCell.append(value);
@@ -138,57 +182,121 @@ $(document).ready(function(){
 	// 	return y;
 	// }
 
-	function setXPositions(x, depth, cells) {
+	//func takes (node, depth)
+	function walkCellsBreadthFirst(cells, depth, func) {
 		var childrenStack = [];
+
 		for (var i = 0; i < cells.length; i++) {
 			if (cells[i] instanceof Array) {
 				childrenStack.push(cells[i]);
 			} else {
-				if (depth > 0) {
-					var cellContainer = cells[i].container;
-					var parentWidth = cellContainer.width();
-					var cellContainerMarginTop = numberOnly(cellContainer.css('margin-top'));
-					var cell = cells[i].cell;
-					console.log(cellContainerMarginTop);
-					var cellHeight = cell.outerHeight() + cellContainerMarginTop;
-					var cellLeftMargin = cells[i].leftMargin;
-
-					cellLeftMargin.css('width', x);
-					cell.css('width', parentWidth - x);
-					cell.css('margin-left', x);
-					cellLeftMargin.css('height', cellHeight);
-
-					var parent = cells[i].parent.cell;
-					var treeVertical = cellLeftMargin.find('.treeVertical');
-					var treeHorizontal = cellLeftMargin.find('.treeHorizontal');
-					var distanceFromParent = cell.offset().top - (parent.offset().top + parent.outerHeight());
-
-					treeVertical.css('right', subCellShiftWidth/2);
-					treeVertical.css('bottom', cellHeight/2);
-					treeVertical.css('height', distanceFromParent + cellHeight/2 - cellContainerMarginTop);
-
-					treeHorizontal.css('width', subCellShiftWidth/2 + treeVertical.width());
-					treeHorizontal.css('top', cellHeight/2);
-					treeHorizontal.css('right', 0);
-				}
+				func(cells[i], depth);
 			}
 		};
 
 		for (var i = 0; i < childrenStack.length; i++) {
-			var xOffset = (depth+1)*subCellShiftWidth;
-			setXPositions(xOffset, depth+1, childrenStack.pop());
+			walkCellsBreadthFirst(childrenStack.shift(), depth+1, func);
 		}
 	}
+
+	var buildLeftMargin = function(node, depth) {
+		if (depth > 0) {
+			var x = (depth)*subCellShiftWidth
+			var cellContainer = node.container;
+			var cell = node.cell;
+			var cellLeftMargin = node.leftMargin;
+			var parent = node.parent.cell;
+			var treeVertical = cellLeftMargin.find('.treeVertical');
+			var treeHorizontal = cellLeftMargin.find('.treeHorizontal');
+
+			var parentWidth = cellContainer.width();
+			var cellContainerMarginTop = numberOnly(cellContainer.css('margin-top'));
+			var cellHeight = cell.outerHeight() + cellContainerMarginTop;
+			var distanceFromParent = cell.offset().top - (parent.offset().top + parent.outerHeight());
+
+			cellLeftMargin.css('width', x);
+			cell.css('width', parentWidth - x);
+			cell.css('margin-left', x);
+			cellLeftMargin.css('height', cellHeight);
+
+			treeVertical.css('right', subCellShiftWidth/2);
+			treeVertical.css('bottom', cellHeight/2);
+			treeVertical.css('height', distanceFromParent + cellHeight/2 - cellContainerMarginTop);
+
+			treeHorizontal.css('width', subCellShiftWidth/2 + treeVertical.width());
+			treeHorizontal.css('top', cellHeight/2);
+			treeHorizontal.css('right', 0);
+		}
+	}
+
+	// function setXPositions(x, depth, cells) {
+	// 	var childrenStack = [];
+	// 	for (var i = 0; i < cells.length; i++) {
+	// 		if (cells[i] instanceof Array) {
+	// 			childrenStack.push(cells[i]);
+	// 		} else {
+	// 			if (depth > 0) {
+	// 				var cellContainer = cells[i].container;
+	// 				var parentWidth = cellContainer.width();
+	// 				var cellContainerMarginTop = numberOnly(cellContainer.css('margin-top'));
+	// 				var cell = cells[i].cell;
+	// 				var cellHeight = cell.outerHeight() + cellContainerMarginTop;
+	// 				var cellLeftMargin = cells[i].leftMargin;
+
+	// 				cellLeftMargin.css('width', x);
+	// 				cell.css('width', parentWidth - x);
+	// 				cell.css('margin-left', x);
+	// 				cellLeftMargin.css('height', cellHeight);
+
+	// 				var parent = cells[i].parent.cell;
+	// 				var treeVertical = cellLeftMargin.find('.treeVertical');
+	// 				var treeHorizontal = cellLeftMargin.find('.treeHorizontal');
+	// 				var distanceFromParent = cell.offset().top - (parent.offset().top + parent.outerHeight());
+
+	// 				treeVertical.css('right', subCellShiftWidth/2);
+	// 				treeVertical.css('bottom', cellHeight/2);
+	// 				treeVertical.css('height', distanceFromParent + cellHeight/2 - cellContainerMarginTop);
+
+	// 				treeHorizontal.css('width', subCellShiftWidth/2 + treeVertical.width());
+	// 				treeHorizontal.css('top', cellHeight/2);
+	// 				treeHorizontal.css('right', 0);
+	// 			}
+	// 		}
+	// 	};
+
+	// 	for (var i = 0; i < childrenStack.length; i++) {
+	// 		var xOffset = (depth+1)*subCellShiftWidth;
+	// 		setXPositions(xOffset, depth+1, childrenStack.pop());
+	// 	}
+	// }
 
 	var hypothesisNode = $('<div class="cellborder" id="hyproot" style="position: relative; width: 80%; margin-left: auto; margin-right: auto;"></div>');
 	$('body').append(hypothesisNode);
 	hypothesisNode.append(buildHeader(hypothesisModel.name, hypothesisModel.author));
-	var cells = walkJSON(hypothesisModel.amplifiers, 0, $("#hyproot"), $("#hyproot"), []);
-	
-	function layoutCells(cells) {
-		setXPositions(0, 0, cells);
-		// setYPositions(0, [0], cells);
+
+	var cells = buildComponentTree(hypothesisModel.amplifiers, $("#hyproot"), $("#hyproot"), []);
+
+	var layoutCells = function() {
+		walkCellsBreadthFirst(cells, 0, buildLeftMargin);
 	}
+
+	var attachClickFunction = function(node, depth) {
+		node.cell.click(function(event) {
+			
+			if (node.isOpen())
+				node.recursiveHideChildren(layoutCells);
+			else
+				node.showChildren(layoutCells);
+		});
+	}
+
+
+	walkCellsBreadthFirst(cells, 0, attachClickFunction);
+	walkCellsBreadthFirst(cells, 0, function(node, depth) {
+		if (depth > 0) {
+			node.container.hide();
+		}
+	})
 
 	layoutCells(cells);
 
