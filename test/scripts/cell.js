b1d12081123f5f4a97cda559bd6902c316a566c3
Weston Beecroft <westoncb@gmail.com>
2014-09-09 04:59:49 -0700

    another test

---
+++ test/scripts/cell.js
@@ -0,0 +1,100 @@
+$(document).ready(function() {
+
+	var cellPattern = createElementPattern('cell');
+	var cellShellPattern = createElementPattern('cellShell');
+	var cellSkinPattern = createElementPattern('cellSkin');
+	var cellUnderbellyPattern = createElementPattern('cellUnderbelly');
+	var cellTopPattern = createElementPattern('cellTop');
+	var cellBodyPattern = createElementPattern('cellBody');
+	var cellBottomPattern = createElementPattern('cellBottom');
+	var cellLeftMarginPattern = createElementPattern('cellLeftMargin');
+	var branchVerticalPattern = createElementPattern('branchVertical');
+	var branchHorizontalPattern = createElementPattern('branchHorizontal');
+
+	Cell.borderTop = 2;
+	Cell.borderRight = 15;
+	Cell.borderBottom = 6;
+	Cell.borderLeft = 5;
+	Cell.topHeight = 14;
+	Cell.bottomHeight = 25;
+	Cell.shellMarginTop = -5;
+
+	function Cell() {
+		this.cellShell = $(cellShellPattern);
+		this.cellSkin = $(cellSkinPattern);
+		this.cellLeftMargin = $(cellLeftMarginPattern);
+		this.cell = $(cellPattern);
+		this.cellUnderbelly = $(cellUnderbellyPattern);
+		this.cellTop = $(cellTopPattern);
+		this.cellBody = $(cellBodyPattern);
+		this.cellBottom = $(cellBottomPattern);
+
+		//assemble the components
+		this.cellShell.append(this.cellLeftMargin);
+		this.cellShell.append(this.cellSkin);
+		this.cellSkin.append(this.cell);
+		this.cellSkin.append(this.cellUnderbelly);
+		this.cell.append(this.cellTop);
+		this.cell.append(this.cellBody);
+		this.cell.append(this.cellBottom);
+
+		//
+		//set any properties that may need to be calculated dynamically in the future
+		//
+		this.cellShell.css('margin-top', Cell.shellMarginTop);
+		this.cellTop.css('height', Cell.topHeight);
+		this.cell.css('padding-top', Cell.topHeight);
+		this.cellTop.append('This is the cell top!');
+
+		this.cellBottom.css('height', Cell.bottomHeight);
+		this.cell.css('padding-bottom', Cell.bottomHeight);
+		this.cellBottom.append('But this... --this is the cell bottom!');
+
+		this.cellBody.css('height', 100);
+
+		this.cell.css('border-top-width', Cell.borderTop);
+		this.cell.css('border-Right-width', Cell.borderRight);
+		this.cell.css('border-Bottom-width', Cell.borderBottom);
+		this.cell.css('border-Left-width', Cell.borderLeft);
+
+		var borderImageSliceValue = ""+Cell.borderTop+" "+Cell.borderRight+" "+Cell.borderBottom+" "+Cell.borderLeft+" fill";
+		this.cell.css('border-image-slice', borderImageSliceValue);
+		this.cell.css('-moz-border-image-slice', borderImageSliceValue);
+		this.cell.css('-webkit-border-image-slice', borderImageSliceValue);
+		this.cell.css('-o-border-image-slice', borderImageSliceValue);
+	}
+
+	Cell.prototype = {
+		getDOMNode: function() {
+			return this.cellShell;
+		},
+
+		getHeight: function() {
+			return this.cellSkin.outerHeight() + Cell.shellMarginTop;	
+		},
+
+		setLeftMarginWidth: function(width) {
+			this.cell.css('margin-left', width);
+			this.cellLeftMargin.css('width', width);
+			this.cellLeftMargin.css('height', this.getHeight());
+		}
+	}
+
+	var cell = new Cell();
+
+	var aTableOrSomething = $("<div style='background-color: white; width: 60%; height: 500px;'></div>");
+	aTableOrSomething.append(cell.getDOMNode());
+	getRootElement().append(aTableOrSomething);
+	
+	function createElementPattern(className, display, id) {
+		var idPart = id ? ('id="' + id + '" ') : '';
+		var displayPart = display ? ('style="display: ' + display + ';"') : '';
+		var pattern = '<div class="' + className + '" ' + idPart + displayPart + "></div>";
+
+		return pattern;
+	}
+
+	function getRootElement() {
+		return $('body');
+	}
+});
---
+++ test/scripts/hypothesis-renderer.js
@@ -0,0 +1,256 @@
+$(document).ready(function(){
+
+	function numberOnly(number) {
+		if (typeof(number) === 'number')
+			return number;
+
+		var endIndex = 1;
+		for (var i = 0; i < number.length; i++) {
+			var character = number[i];
+			if ((typeof(character) === 'number' && typeof(character) !== NaN) || character === '-' || character === '+' || character === '.') {
+				endIndex++;
+			} else
+				break;
+		}
+
+		return parseInt(number.substring(0, endIndex));
+	}
+
+	var hypothesisModelString = '{"id": "0-0", \
+					  "name": "The moon is made of cheese. Here\'s some CHEESE for ya bra!", \
+					  "author": "Sofia Chandler-Freed", \
+					  "amplifiers": [ \
+					  				{ \
+					  				"id": "1-0", \
+					  				"type": "justification", \
+					  				"contents": "Here is some more text. And it\'s not just any text, but THE very best text possible--especially since it\'s about how the moon\'s made out of some kinda weird ass soy-based Wallace and Grommit cheese.catscatscatscatscats catscatscatscatscats catscatscatscatscat scatscatscatscatscatscats catscatscatscatscatscatscatscatsca tscatscatscatscat scatscatscatscatscatscatscatscatscatscats catscatscatscatscatsc atscatscatscatscat scatscatscatscatscat scatscatscatscatsca tscatscatscatscatsc atscats", \
+					  				"amplifiers": "" \
+					  			  }, \
+					  			  { \
+					  				"id": "1-1", \
+					  				"type": "justification", \
+					  				"contents": "catscatscatscatscatscatscatscatscatscatscatscatscatscatscatscatscatscatscatscatscats catscatscatscatscatscatscatscatscatscatscatscatscatscatscatscatscatscatscatscatscatscats catscatscatscatscatscatscatscatscatscatscatscatscatscats", \
+					  				"amplifiers": [ \
+						  				{ \
+						  				"id": "2-0", \
+						  				"type": "justification", \
+						  				"contents": "Here is some more text. And it\'s not just any text, but THE very best text possible--especially since it\'s about how the moon\'s made out of some kinda weird ass soy-based Wallace and Grommit cheese.", \
+						  				"amplifiers": [ \
+							  				{ \
+							  				"id": "3-0", \
+							  				"type": "justification", \
+							  				"contents": "Here is some more text. And it\'s not just any text, but THE very best text possible--especially since it\'s about how the moon\'s made out of some kinda weird ass soy-based Wallace and Grommit cheese.", \
+							  				"amplifiers": "" \
+							  			  }, \
+							  			  { \
+							  				"id": "3-1", \
+							  				"type": "justification", \
+							  				"contents": "catscatscatscatscatscatscatscatscatscatscatscatscatscatscatscatscatscatscatscatscats catscatscatscatscatscatscatscatscatscatscatscatscatscatscatscatscatscatscatscatscatscats catscatscatscatscatscatscatscatscatscatscatscatscatscats", \
+							  				"amplifiers": [ \
+								  				{ \
+								  				"id": "4-0", \
+								  				"type": "justification", \
+								  				"contents": "Here is some more text. And it\'s not just any text, but THE very best text possible--especially since it\'s about how the moon\'s made out of some kinda weird ass soy-based Wallace and Grommit cheese.", \
+								  				"amplifiers": "" \
+								  			  }, \
+								  			  { \
+								  				"id": "4-1", \
+								  				"type": "justification", \
+								  				"contents": "catscatscatscatscatscatscatscatscatscatscatscatscatscatscatscatscatscatscatscatscats catscatscatscatscatscatscatscatscatscatscatscatscatscatscatscatscatscatscatscatscatscats catscatscatscatscatscatscatscatscatscatscatscatscatscats", \
+								  				"amplifiers": "" \
+								  			  }] \
+							  			  }] \
+						  			  }, \
+						  			  { \
+						  				"id": "2-1", \
+						  				"type": "justification", \
+						  				"contents": "catscatscatscatscatscatscatscatscatscatscatscatscatscatscatscatscatscatscatscatscats catscatscatscatscatscatscatscatscatscatscatscatscatscatscatscatscatscatscatscatscatscats catscatscatscatscatscatscatscatscatscatscatscatscatscats", \
+						  				"amplifiers": "" \
+						  			  }] \
+					  			  }] \
+					 }';
+
+	var hypothesisModel = jQuery.parseJSON(hypothesisModelString);
+	var cellString = '<div class="cellborder"></div>';
+	var cellLeftMarginString = '<div class="cellleftmargin"><div class="treeVertical"></div><div class="treeHorizontal"></div></div>'; 
+	var cellContainerString = '<div class="cellcontainer"></div>';
+	const subCellShiftWidth = 50;
+
+	function buildComponentTree(json, root, parent, cells) {
+		if (json instanceof Array) {
+			for (var element in json) {
+				buildComponentTree(json[element], root, parent, cells);	
+			}
+		} else {
+			var thisCell = $(cellString);
+			var thisContainer = $(cellContainerString);
+			var thisCellLeftMargin = $(cellLeftMarginString);
+			var atTheRoot = (root === parent);
+
+			var node = {
+				container: thisContainer,
+				leftMargin: thisCellLeftMargin,
+				cell: thisCell,
+				parent: (atTheRoot ? 0 : parent),
+				childSet: [],
+				isOpen: function() {
+					if (this.childSet.length == 0)
+						return true;
+
+					var isit = this.childSet[0].container.is(":visible");
+					return isit;
+				},
+
+				recursiveHideChildren: function(func) {
+					for (var i = 0; i < this.childSet.length; i++) {
+						var child = this.childSet[i];
+						// child.container.slideUp({progress: func,
+						// 						 duration: 300});
+						child.container.hide();
+						child.recursiveHideChildren();
+					};
+				},
+
+				recursiveShowChildren: function() {
+					for (var i = 0; i < this.childSet.length; i++) {
+						var child = this.childSet[i];
+						child.container.show();
+						child.recursiveShowChildren();
+					};
+				},
+
+				showChildren: function(func) {
+					for (var i = 0; i < this.childSet.length; i++) {
+						var child = this.childSet[i];
+							// child.container.slideDown({progress: func,
+							// 						 duration: 300});
+						child.container.show();
+					};	
+				},
+
+				hideChildren: function() {
+					for (var i = 0; i < this.childSet.length; i++) {
+						var child = this.childSet[i];
+						child.container.hide();
+					};	
+				}
+			};
+
+			if (node.parent.childSet)
+				node.parent.childSet.push(node);
+
+			thisContainer.append(thisCellLeftMargin);
+			thisContainer.append(thisCell);
+			root.append(thisContainer);
+			cells.push(node);
+
+			for (var key in json) {
+	    		thisCell.append(key + ": ");
+
+		    	var value = json[key]
+		    	if (typeof(value) == "object") {
+	    			var subCells = buildComponentTree(value, root, node, []);
+	    			cells.push(subCells);
+		    	} else {
+    				thisCell.append(value);
+    				thisCell.append("<br>");
+		    	}
+		    }
+		}
+
+		return cells;
+	}
+
+	function buildHeader(title, author) {
+		return '<div id="title-section"> \
+				<span class="hypothesis-title-word">Hypothesis:</span> <span class="hypothesis-title">' + title + '</span> \
+				<div id="author-text">by ' + author + '</div> \
+			</div><div class="sep1"></div><br>';
+	}
+
+	//func takes (node, depth)
+	function walkCellsBreadthFirst(cells, depth, func) {
+		var childrenStack = [];
+
+		for (var i = 0; i < cells.length; i++) {
+			if (cells[i] instanceof Array) {
+				childrenStack.push(cells[i]);
+			} else {
+				func(cells[i], depth);
+			}
+		};
+
+		for (var i = 0; i < childrenStack.length; i++) {
+			walkCellsBreadthFirst(childrenStack.shift(), depth+1, func);
+		}
+	}
+
+	var buildLeftMargin = function(node, depth) {
+		if (depth > 0) {
+			var x = (depth)*subCellShiftWidth
+			var cellContainer = node.container;
+			var cell = node.cell;
+			var cellLeftMargin = node.leftMargin;
+			var parent = node.parent.cell;
+			var treeVertical = cellLeftMargin.find('.treeVertical');
+			var treeHorizontal = cellLeftMargin.find('.treeHorizontal');
+
+			var parentWidth = cellContainer.width();
+			var cellContainerMarginTop = numberOnly(cellContainer.css('margin-top'));
+			var cellHeight = cell.outerHeight() + cellContainerMarginTop;
+			var distanceFromParent = cell.offset().top - (parent.offset().top + parent.outerHeight());
+
+			cellLeftMargin.css('width', x);
+			cell.css('width', parentWidth - x);
+			cell.css('margin-left', x);
+			cellLeftMargin.css('height', cellHeight);
+
+			treeVertical.css('right', subCellShiftWidth/2);
+			treeVertical.css('bottom', cellHeight/2);
+			treeVertical.css('height', distanceFromParent + cellHeight/2 - cellContainerMarginTop);
+
+			treeHorizontal.css('width', subCellShiftWidth/2 + treeVertical.width());
+			treeHorizontal.css('top', cellHeight/2);
+			treeHorizontal.css('right', 0);
+		}
+	}
+
+	var hypothesisNode = $('<div class="cellborderroot" id="hyproot" style="position: relative; width: 80%; margin-left: auto; margin-right: auto;"></div>');
+	$('body').append(hypothesisNode);
+	hypothesisNode.append(buildHeader(hypothesisModel.name, hypothesisModel.author));
+
+	var cells = buildComponentTree(hypothesisModel.amplifiers, $("#hyproot"), $("#hyproot"), []);
+
+	var layoutCells = function() {
+		walkCellsBreadthFirst(cells, 0, buildLeftMargin);
+	}
+
+	var attachClickFunction = function(node, depth) {
+		node.cell.click(function(event) {
+			
+			if (node.isOpen())
+				node.recursiveHideChildren(layoutCells);
+			else
+				node.showChildren(layoutCells);
+
+			layoutCells();
+		});
+	}
+
+
+	walkCellsBreadthFirst(cells, 0, attachClickFunction);
+	walkCellsBreadthFirst(cells, 0, function(node, depth) {
+		if (depth > 0) {
+			node.container.hide();
+		}
+	})
+
+	layoutCells(cells);
+
+	$(window).resize(function() {
+		layoutCells(cells);
+	});
+	$(window).load(function() {
+		layoutCells(cells);
+	});
+});
